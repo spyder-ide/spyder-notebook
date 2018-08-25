f5f0ac85d88c93022e57222d08a1e5869e968dbd
Jitse Niesen <jitseniesen@yahoo.com>
2018-08-25 12:43:46 +0100

    Add -x -vv flags to pytest invocation

--- .circleci/run-tests.sh
+++ .circleci/run-tests.sh
@@ -3,5 +3,9 @@
 
 export PATH="$HOME/miniconda/bin:$PATH"
 mkdir test-reports
-pytest spyder_notebook --cov=spyder_notebook --junitxml=test-reports/junit.xml
+
+# Run tests; flag -x means stop on first test failure 
+pytest -x -vv spyder_notebook --cov=spyder_notebook --junitxml=test-reports/junit.xml
+
+# Generate coverage report
 COVERALLS_REPO_TOKEN=Kr503QwklmJYKXYRXLywrtw8zbX7K8SKx coveralls
