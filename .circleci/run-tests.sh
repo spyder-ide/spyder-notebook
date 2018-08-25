743ac84a36e197832499143e73a5dde5ebbc2fb3
Jitse Niesen <jitseniesen@yahoo.com>
2018-08-25 16:30:43 +0100

    Do not upload test results on CircleCI

--- .circleci/run-tests.sh
+++ .circleci/run-tests.sh
@@ -3,10 +3,9 @@
 # -x means:  Print commands and their arguments as they are executed.
 
 export PATH="$HOME/miniconda/bin:$PATH"
-mkdir test-reports
 
 # Run tests; flag -x means stop on first test failure 
-pytest -x -vv spyder_notebook --cov=spyder_notebook --junitxml=test-reports/junit.xml
+pytest -x -vv spyder_notebook --cov=spyder_notebook
 
 # Generate coverage report
 COVERALLS_REPO_TOKEN=Kr503QwklmJYKXYRXLywrtw8zbX7K8SKx coveralls
