6403ab2b652f7025a34200623cc7cafe55c702ec
Jitse Niesen <jitseniesen@yahoo.com>
2020-01-13 14:30:31 +0000

    Add test, ensure client is not destroyed too early in tests
    We need to keep a reference to the plugin in the tests because otherwise
    the plugin and the client (which is a Qt child of the plugin) gets
    destroyed.

--- spyder_notebook/widgets/tests/test_client.py
+++ spyder_notebook/widgets/tests/test_client.py
@@ -8,6 +8,7 @@
 # Third-party imports
 import pytest
 from qtpy.QtWidgets import QWidget
+import requests
 
 # Local imports
 from spyder_notebook.widgets.client import NotebookClient
@@ -17,38 +18,59 @@ class MockPlugin(QWidget):
     def get_plugin_actions(self):
         return []
 
-
 @pytest.fixture
-def client(qtbot):
-    """Construct a NotebookClient for use in tests."""
+def plugin(qtbot):
+    """
+    Construct mock plugin with NotebookClient for use in tests.
+
+    Use `plugin.client` to access the client.
+    """
     plugin = MockPlugin()
+    qtbot.addWidget(plugin)
     client = NotebookClient(plugin, '/path/notebooks/ham.ipynb')
+    plugin.client = client
     server_info = {'notebook_dir': '/path/notebooks',
                    'url': 'fake_url',
                    'token': 'fake_token'}
     client.register(server_info)
-    return client
+    return plugin
 
 
-def test_notebookclient_get_kernel_id(client, mocker):
+def test_notebookclient_get_kernel_id(plugin, mocker):
     """Basic unit test for NotebookClient.get_kernel_id()."""
     response = mocker.Mock()
     content = b'[{"kernel": {"id": "42"}, "notebook": {"path": "ham.ipynb"}}]'
     response.content = content
+    response.status_code = requests.codes.ok
     mocker.patch('requests.get', return_value=response)
 
-    kernel_id = client.get_kernel_id()
+    kernel_id = plugin.client.get_kernel_id()
     assert kernel_id == '42'
 
 
-def test_notebookclient_get_kernel_id_with_fields_missing(client, mocker):
+def test_notebookclient_get_kernel_id_with_fields_missing(plugin, mocker):
     """Test NotebookClient.get_kernel_id() if response has fields missing."""
     response = mocker.Mock()
     content = (b'[{"kernel": {"id": "1"}, "notebook": {"spam": "eggs"}},'
                b' {"kernel": {"id": "2"}},'
                b' {"kernel": {"id": "3"}, "notebook": {"path": "ham.ipynb"}}]')
     response.content = content
+    response.status_code = requests.codes.ok
     mocker.patch('requests.get', return_value=response)
 
-    kernel_id = client.get_kernel_id()
+    kernel_id = plugin.client.get_kernel_id()
     assert kernel_id == '3'
+
+
+def test_notebookclient_get_kernel_id_with_error_status(plugin, mocker):
+    """Test NotebookClient.get_kernel_id() when response has error status."""
+    response = mocker.Mock()
+    content = b'{"message": "error"}'
+    response.content = content
+    response.status_code = requests.codes.forbidden
+    mocker.patch('requests.get', return_value=response)
+    MockMessageBox = mocker.patch('spyder_notebook.widgets.client.QMessageBox')
+
+    plugin.client.get_kernel_id()
+
+    MockMessageBox.warning.assert_called()
