d4bdd1dbb54890a19f6de35b000630d5f886b87d
Jitse Niesen <jitseniesen@yahoo.com>
2020-05-27 18:55:54 +0100

    Tests: Remove code specific to Python 2
    This code was left behind when support for Python 2 was dropped.

--- spyder_notebook/tests/test_plugin.py
+++ spyder_notebook/tests/test_plugin.py
@@ -26,10 +26,6 @@ from spyder.config.base import get_home_dir
 # Local imports
 from spyder_notebook.notebookplugin import NotebookPlugin
 
-# Python 2 compatibility
-if sys.version_info[0] == 2:
-    PermissionError = OSError
-
 # =============================================================================
 # Constants
 # =============================================================================
