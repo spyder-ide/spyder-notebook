57896ff4145321febdedfb1e4443ffbd82327d65
Jitse Niesen <jitseniesen@yahoo.com>
2020-05-30 17:27:59 +0100

    Widgets: Don't reopen tabs with new notebooks after closing them
    Before, if the user closes a tab with a newly created notebook and
    chooses to save it under a different name when prompted, a new tab would
    be opened with the new file name. This commit prevents the opening of the
    new tab.

--- spyder_notebook/widgets/notebooktabwidget.py
+++ spyder_notebook/widgets/notebooktabwidget.py
@@ -258,17 +258,17 @@ class NotebookTabWidget(Tabs):
         answer = QMessageBox.question(
             self, _('Save changes'), text, buttons)
         if answer == QMessageBox.Yes:
-            self.save_as(close_after_save=False)
+            self.save_as(reopen_after_save=False)
 
-    def save_as(self, name=None, close_after_save=True):
+    def save_as(self, name=None, reopen_after_save=True):
         """
         Save current notebook under a different file name.
 
         First, save the notebook under the original file name. Then ask user
         for a new file name (if `name` is not set), and return if no new name
         is given. Then, read the contents of the notebook that was just saved
-        and write them under the new file name. Finally. close the original
-        tab (unless `close_after_save` is False) and open a new tab with the
+        and write them under the new file name. If `reopen_after_save` is
+        True, then close the original tab and open a new tab with the
         notebook loaded from the new file name.
 
         Parameters
@@ -276,9 +276,9 @@ class NotebookTabWidget(Tabs):
         name : str or None, optional
             File name under which the notebook is to be saved. The default is
             None, meaning that the user should be asked for the file name.
-        close_after_save : bool, optional
-            Whether to close the original tab after saving the notebook and
-            before opening it under the new name. The default is True.
+        reopen_after_save : bool, optional
+            Whether to close the original tab and re-open it under the new
+            file name after saving the notebook. The default is True.
         """
         current_client = self.currentWidget()
         current_client.save()
@@ -304,9 +304,9 @@ class NotebookTabWidget(Tabs):
                        .format(filename, str(error)))
                 QMessageBox.critical(self, _("File Error"), txt)
                 return
-            if close_after_save:
+            if reopen_after_save:
                 self.close_client(save_before_close=False)
-            self.create_new_client(filename=filename)
+                self.create_new_client(filename=filename)
 
     def add_tab(self, widget):
         """
