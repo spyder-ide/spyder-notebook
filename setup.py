9bc1f80abe8da9610558e7ba0cfc4951859aaa2a
Jitse Niesen <jitseniesen@yahoo.com>
2020-07-11 14:29:16 +0100

    Packaging: Build JavaScript in setup.py and include in package

--- setup.py
+++ setup.py
@@ -1,25 +1,41 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright © Spyder Project Contributors
+# Copyright (c) Spyder Project Contributors
 # Licensed under the terms of the MIT License
-# (see spyder_notebook/__init__.py for details)
 
-
-"""
-Spyder notebook plugin
-======================
-
-Jupyter notebook integration with Spyder.
-"""
+"""Setup script for spyder_notebook."""
 
 # Standard library imports
 import ast
+import distutils
 import os
+import os.path as osp
+import pipes
+import subprocess
+import sys
 
 # Third party imports
 from setuptools import find_packages, setup
+from setuptools.command.sdist import sdist
+
+if sys.platform == 'win32':
+    from subprocess import list2cmdline
+else:
+    def list2cmdline(cmd_list):
+        """Convert list of arguments to a command line string."""
+        return ' '.join(map(pipes.quote, cmd_list))
+
 
 HERE = os.path.abspath(os.path.dirname(__file__))
+SERVER_DIR = osp.join(HERE, 'spyder_notebook', 'server')
+BUILD_DIR = osp.join(SERVER_DIR, 'build')
+
+
+def run(cmd, *args, **kwargs):
+    """Echo a command before running it."""
+    distutils.log.info('> ' + list2cmdline(cmd))
+    kwargs['shell'] = (sys.platform == 'win32')
+    return subprocess.check_call(cmd, *args, **kwargs)
 
 
 def get_version(module='spyder_notebook'):
@@ -35,12 +51,32 @@ def get_version(module='spyder_notebook'):
     return version
 
 
+class my_sdist(sdist):
+    """Extension for the default sdist command."""
+
+    def run(self):
+        """Copmpile JavaScript and then do the default run()."""
+        if not osp.isdir(BUILD_DIR):
+            run(['jlpm', 'install'], cwd=SERVER_DIR)
+            run(['jlpm', 'build'], cwd=SERVER_DIR)
+        sdist.run(self)
+
+
+# Verify that BUILD_DIR exist before trying to build wheels
+if any([arg == 'bdist_wheel' for arg in sys.argv]):
+    if not osp.isdir(BUILD_DIR):
+        print("\nERROR: Server components are missing!! Please run "
+              "'python setup.py sdist' first.\n")
+        sys.exit(1)
+
+
 REQUIREMENTS = ['spyder>=4.1,<5', 'jinja2', 'jupyter_core', 'nbformat',
                 'notebook>=4.3', 'qtpy', 'qdarkstyle', 'requests', 'traitlets']
 
 setup(
     name='spyder-notebook',
     version=get_version(),
+    cmdclass={'sdist': my_sdist},
     keywords='spyder jupyter notebook',
     python_requires='>=3.5',
     url='https://github.com/spyder-ide/spyder-notebook',
---
+++ spyder_notebook/server/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) Jupyter Development Team, Spyder Project Contributors.
+# Distributed under the terms of the Modified BSD License.
+
+"""Notebook server for the Spyder notebook plugin."""
