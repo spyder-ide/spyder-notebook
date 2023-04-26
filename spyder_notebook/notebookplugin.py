fb6c9334f8444934a2cc0e13607a2467a727b98c
Jitse Niesen <jitseniesen@yahoo.com>
2023-04-26 12:35:37 +0100

    Move interaction with switcher from widget to plugin

--- spyder_notebook/notebookplugin.py
+++ spyder_notebook/notebookplugin.py
@@ -5,13 +5,14 @@
 
 """Notebook plugin."""
 
-# Qt imports
-from qtpy.QtCore import Signal
+# Standard library imports
+import os.path as osp
 
 # Spyder imports
 from spyder.api.plugins import Plugins, SpyderDockablePlugin
 from spyder.api.plugin_registration.decorators import (
     on_plugin_available, on_plugin_teardown)
+from spyder.plugins.switcher.utils import shorten_paths
 
 # Local imports
 from spyder_notebook.config import CONF_DEFAULTS, CONF_VERSION
@@ -25,7 +26,7 @@ class NotebookPlugin(SpyderDockablePlugin):
 
     NAME = 'notebook'
     REQUIRES = [Plugins.Preferences]
-    OPTIONAL = [Plugins.IPythonConsole]
+    OPTIONAL = [Plugins.IPythonConsole, Plugins.Switcher]
     TABIFY = [Plugins.Editor]
     CONF_SECTION = NAME
     CONF_DEFAULTS = CONF_DEFAULTS
@@ -63,6 +64,12 @@ class NotebookPlugin(SpyderDockablePlugin):
         self.get_widget().sig_open_console_requested.connect(
             self._open_console)
 
+    @on_plugin_available(plugin=Plugins.Switcher)
+    def on_switcher_available(self):
+        switcher = self.get_plugin(Plugins.Switcher)
+        switcher.sig_mode_selected.connect(self._handle_switcher_modes)
+        switcher.sig_item_selected.connect(self._handle_switcher_selection)
+
     @on_plugin_teardown(plugin=Plugins.Preferences)
     def on_preferences_teardown(self):
         preferences = self.get_plugin(Plugins.Preferences)
@@ -72,6 +79,12 @@ class NotebookPlugin(SpyderDockablePlugin):
     def on_ipyconsole_teardown(self):
         self.get_widget().sig_open_console_requested.disconnect(
             self._open_console)
+
+    @on_plugin_teardown(plugin=Plugins.Switcher)
+    def on_switcher_teardown(self):
+        switcher = self.get_plugin(Plugins.Switcher)
+        switcher.sig_mode_selected.disconnect(self._handle_switcher_modes)
+        switcher.sig_item_selected.disconnect(self._handle_switcher_selection)
 
     def on_mainwindow_visible(self):
         self.get_widget().open_previous_session()
@@ -88,3 +101,56 @@ class NotebookPlugin(SpyderDockablePlugin):
         ipyclient = ipyconsole.get_current_client()
         ipyclient.allow_rename = False
         ipyconsole.rename_client_tab(ipyclient, tab_name)
+
+    def _handle_switcher_modes(self, mode):
+        """
+        Populate switcher with opened notebooks.
+
+        List the file names of the opened notebooks with their directories in
+        the switcher. Only handle file mode, where `mode` is empty string.
+        """
+        if mode != '':
+            return
+
+        tabwidget = self.get_widget().tabwidget
+        clients = [tabwidget.widget(i) for i in range(tabwidget.count())]
+        paths = [client.get_filename() for client in clients]
+        is_unsaved = [False for client in clients]
+        short_paths = shorten_paths(paths, is_unsaved)
+        icon = self.create_icon('notebook')
+        section = self.get_name()
+        switcher = self.get_plugin(Plugins.Switcher)
+
+        for path, short_path, client in zip(paths, short_paths, clients):
+            title = osp.basename(path)
+            description = osp.dirname(path)
+            if len(path) > 75:
+                description = short_path
+            is_last_item = (client == clients[-1])
+
+            switcher.add_item(
+                title=title,
+                description=description,
+                icon=icon,
+                section=section,
+                data=client,
+                last_item=is_last_item
+            )
+
+    def _handle_switcher_selection(self, item, mode, search_text):
+        """
+        Handle user selecting item in switcher.
+
+        If the selected item is not in the section of the switcher that
+        corresponds to this plugin, then ignore it. Otherwise, switch to
+        selected item in notebook plugin and hide the switcher.
+        """
+        if item.get_section() != self.get_title():
+            return
+
+        client = item.get_data()
+        tabwidget = self.get_widget().tabwidget
+        tabwidget.setCurrentIndex(tabwidget.indexOf(client))
+        self.switch_to_plugin()
+        switcher = self.get_plugin(Plugins.Switcher)
+        switcher.hide()
