1a36bc28a9d54d1c01a857e4e93941498b466162
Jitse Niesen <jitseniesen@yahoo.com>
2024-05-08 20:24:28 +0100

    Fix test_open_console_when_no_kernel and move to test_main_widget.py
    * The test that no console was opened did not work because this can be
      done asynchronously, so instead test for the signal requesting that
      the console be opened.
    * The test now only needs the main widget, not the plugin, so move it
      accordingly.

--- spyder_notebook/widgets/tests/test_main_widget.py
+++ spyder_notebook/widgets/tests/test_main_widget.py
@@ -205,6 +205,34 @@ def test_shutdown_notebook_kernel(main_widget, qtbot):
     assert not is_kernel_up(kernel_id, sessions_url)
 
 
+@flaky
+def test_open_console_when_no_kernel(main_widget, qtbot, mocker):
+    """Test that open_console() handles the case when there is no kernel."""
+    # Create mock QMessageBox
+    MockMessageBox = mocker.patch(
+        'spyder_notebook.widgets.main_widget.QMessageBox')
+
+    # Create new notebook tab and wait for prompt
+    main_widget.create_new_client()
+    client = main_widget.tabwidget.currentWidget()
+    nbwidget = client.notebookwidget
+    qtbot.waitUntil(lambda: prompt_present(nbwidget, qtbot),
+                    timeout=NOTEBOOK_UP)
+
+    # Shut the kernel down and check that this is successful
+    kernel_id = client.get_kernel_id()
+    sessions_url = client.get_session_url()
+    client.shutdown_kernel()
+    assert not is_kernel_up(kernel_id, sessions_url)
+
+    # Try opening a console and check corresponding signal is not emitted
+    with qtbot.assertNotEmitted(main_widget.sig_open_console_requested):
+        main_widget.open_console(client)
+
+    # Assert that a dialog is displayed and no console was opened
+    MockMessageBox.critical.assert_called()
+
+
 def test_file_in_temp_dir_deleted_after_notebook_closed(main_widget, qtbot):
     """Test that notebook file in temporary directory is deleted after the
     notebook is closed."""
