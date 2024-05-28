9f42b4a18765404acde1dd4e1810ced3efd54330
Jitse Niesen <jitseniesen@yahoo.com>
2024-05-28 06:39:06 +0100

    Move import in conftest.py inside functions
    As a side-effect of PR spyder-ide/spyder#21734, a QApplication needs
    to be running before importing
    spyder.plugins.preferences.widgets.configdialog. Moving the import
    inside a function achieves this.

--- spyder_notebook/tests/conftest.py
+++ spyder_notebook/tests/conftest.py
@@ -27,7 +27,6 @@ from spyder.api.plugins import Plugins
 from spyder.api.plugin_registration.registry import PLUGIN_REGISTRY
 from spyder.app.cli_options import get_options
 from spyder.config.manager import CONF
-from spyder.plugins.preferences.plugin import Preferences
 from spyder.utils.icon_manager import ima
 
 
@@ -35,6 +34,10 @@ class MainWindowMock(QMainWindow):
     register_shortcut = Mock()
 
     def __init__(self, parent):
+        # This import assumes that an QApplication is already running,
+        # so we can not put it at the top of the file
+        from spyder.plugins.preferences.plugin import Preferences
+
         super().__init__(parent)
         self.default_style = None
         self.widgetlist = []
@@ -76,6 +79,10 @@ class MainWindowMock(QMainWindow):
 class ConfigDialogTester(QWidget):
     def __init__(self, parent, main_class,
                  general_config_plugins, plugins):
+        # This import assumes that an QApplication is already running,
+        # so we can not put it at the top of the file
+        from spyder.plugins.preferences.plugin import Preferences
+
         super().__init__(parent)
         self._main = main_class(self) if main_class else None
         if self._main is None:
